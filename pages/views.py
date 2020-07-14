c468caead537e17f69292391d2759c65af6d99f9
Daniil Vilyavin <89505105005@mail.ru>
2020-07-14 18:55:17 +0300

    Complete working with this project

--- pages/views.py
+++ pages/views.py
@@ -2,5 +2,4 @@ from django.shortcuts import render
 
 
 def index(request):
-    # compiler.execute_compiler("./site_level_configuration_file.yaml", "./AUGMENTED_SITE_LEVEL_CONFIG_FILE.yaml", ".tmp")
     return render(request, 'index.html')
